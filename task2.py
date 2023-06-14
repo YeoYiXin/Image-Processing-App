10a2c53b886623afbedc0a33f64412ab3e14aff0
YeoYiXin <89788614+YeoYiXin@users.noreply.github.com>
2023-06-14 17:21:01 +0800

    Delete task2.py

--- task2.py
+++
@@ -1,8 +0,0 @@
-#2.	Create a second programme which takes in an input image and change its colours to greyscale (black and white). 
-#Do NOT use the built-in libraries to do this - instead, perform the calculation and use the results obtained 
-#to produce the new output image. 
-#This will get you both familiarise with how to manipulate images, a useful skill that will benefit you both later on.
-
-
-
-
